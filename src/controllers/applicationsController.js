d8068f9d26cbf344bf18fd662b07bee12408c3ad
DaniilKOT112 <user1user2user3@mail.ru>
2025-05-14 20:10:24 +0300

    новый router getAdoption acceptAdoption cancelAdoption deleteAdoption

--- src/controllers/applicationsController.js
+++ src/controllers/applicationsController.js
@@ -100,5 +100,103 @@ const deleteContent = async (req, res) => {
     }
 }
 
+const getAdoption = async (req, res) => {
+    const { text, user_id } = req.query;
 
-module.exports = {getContent, acceptContent, cancelContent, deleteContent};
+    try {
+        let baseQuery = `
+            SELECT aa.id_app, aa.first_name, aa.last_name, aa.telephone, aa.user_id, aa.creator, p.id_pets, p.nickname as pets, 
+                   TO_CHAR(aa.date, 'YYYY-MM-DD') AS date, sa.id_status_adoption, sa.name_status as status 
+            FROM "AdoptionApp" aa
+            LEFT JOIN "Pets" p ON aa.pets_id = p.id_pets
+            LEFT JOIN "StatusAdoption" sa ON aa.status_adoption_id = sa.id_status_adoption
+            WHERE aa.user_id = $1`;
+
+        let params = [user_id];
+
+        if (text && text.trim() !== '') {
+            baseQuery += ` AND (CAST(aa.id_app AS TEXT) ILIKE $2 OR aa.first_name ILIKE $2 OR aa.last_name ILIKE $2 OR aa.telephone ILIKE $2 
+            OR p.nickname ILIKE $2 OR TO_CHAR(aa.date, 'YYYY-MM-DD') ILIKE $2 OR sa.name_status ILIKE $2)`;
+            params.push( `%${text}%` );
+        }
+
+        baseQuery += ` ORDER BY aa.first_name ASC`;
+
+        const result = await pool.query(baseQuery, params);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: 'Данные отсутствуют!' });
+        }
+        return res.status(200).json({ message: 'Данные получены!', data: result.rows });
+    } catch (err) {
+        console.error('Ошибка при выполнении запроса:', err);
+        return res.status(500).json({ message: 'Не удалось вернуть данные!' });
+    }
+}
+
+const acceptAdoption = async (req, res) => {
+    const { creator, message, user_id, status, id_app } = req.body;
+
+    try {
+        await pool.query('BEGIN');
+        const result = await pool.query(
+            'INSERT INTO "Messages" (creator, message, user_id, status)' +
+            'VALUES ($1, $2, $3, $4) RETURNING *',
+            [creator, message, user_id, status]
+        );
+
+        await pool.query('UPDATE "AdoptionApp" SET status_adoption_id = 2 WHERE id_app = $1', [id_app])
+        await pool.query('COMMIT');
+
+        broadcast({event:'message-accept-adoption', data: result.rows[0]});
+        return res.status(201).json({message: 'Сообщение отправлено!', network: result.rows[0]});
+    } catch (err) {
+        await pool.query('ROLLBACK');
+        console.error(err);
+        return res.status(500).json({message: 'Ошибка отправки сообщения!'});
+    }
+}
+
+const cancelAdoption = async (req, res) => {
+    const { creator, message, user_id, status, id_app } = req.body;
+
+    try {
+        await pool.query('BEGIN');
+        const result = await pool.query(
+            'INSERT INTO "Messages" (creator, message, user_id, status)' +
+            'VALUES ($1, $2, $3, $4) RETURNING *',
+            [creator, message, user_id, status]
+        );
+
+        await pool.query('UPDATE "AdoptionApp" SET status_adoption_id = 0 WHERE id_app = $1', [id_app])
+        await pool.query('COMMIT');
+
+        broadcast({event:'message-cancel-adoption', data: result.rows[0]});
+        return res.status(201).json({message: 'Сообщение отправлено!', network: result.rows[0]});
+    } catch (err) {
+        await pool.query('ROLLBACK');
+        console.error(err);
+        return res.status(500).json({message: 'Ошибка отправки сообщения!'});
+    }
+}
+
+const deleteAdoption = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const adoptionExists = await pool.query(
+            'SELECT * FROM "AdoptionApp" WHERE id_app = $1', [id]
+        );
+
+        if (adoptionExists.rows.length === 0) {
+            return res.status(404).json({message: 'Такой заявки не существует!'});
+        }
+
+        await pool.query('DELETE FROM "AdoptionApp" WHERE id_app = $1', [id]);
+        return res.status(200).json({message: 'Удаление заявки выполнено успешно!'});
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({message: 'Возникла ошибка при удалении заявки!'});
+    }
+}
+
+module.exports = {getContent, acceptContent, cancelContent, deleteContent, getAdoption, cancelAdoption, acceptAdoption, deleteAdoption};
--- src/routes/applicationsRouter.js
+++ src/routes/applicationsRouter.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const {getContent, acceptContent, cancelContent, deleteContent} = require('../controllers/applicationsController');
+const {getContent, acceptContent, cancelContent, deleteContent, getAdoption,
+    deleteAdoption, acceptAdoption, cancelAdoption} = require('../controllers/applicationsController');
 
 router.get('/get-content', getContent);
 router.post('/accept-content', acceptContent);
 router.post('/cancel-content', cancelContent);
 router.delete('/delete-content/:id', deleteContent);
+router.get('/get-adoption', getAdoption);
+router.post('/accept-adoption', acceptAdoption);
+router.post('/cancel-adoption', cancelAdoption);
+router.delete('/delete-adoption/:id', deleteAdoption);
 
 module.exports = router;
